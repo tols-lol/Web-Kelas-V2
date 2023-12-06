901e82694baa53697bc30a1b7d546b2419f5b615
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2023-12-06 23:24:49 +0700

    Add files via upload

---
+++ src/components/Mapel/Senin.jsx
@@ -0,0 +1,33 @@
+const Senin = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Bahasa Daerah</div>
+					<div className="flex justify-center items-center text-sm">07.35-08.45</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Matematika</div>
+					<div className="flex justify-center items-center text-sm">08.45-09.20</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">09.20-9.40</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Matematika</div>
+					<div className="flex justify-center items-center text-sm">09.40-11.30</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">11.30-12.10</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Sejarah</div>
+					<div className="flex justify-center items-center text-sm">12.10-1.00</div>
+				</div>
+        </>
+    )
+} 
+
+export default Senin;
