c5fadc42d10610bd0996b506016f2206fa4b615c
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2023-12-06 23:46:58 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -21,6 +21,7 @@ const Jumat = () => {
 				data-aos-duration="700">
 				<div className="w-[50%] text-base  font-medium">Istirahat</div>
 				<div className="flex justify-center items-center text-sm">09.20-09.40</div>
+			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
