481d9364b5c9900274edee3a97187963e04e592a
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2023-12-06 23:34:55 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,12 +2,12 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://instagram.com/x_artm_">
+			<a href="wa.me/6283188140830">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-base font-semibold pr-0 mt-5">Instagram</h1>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Whatsapp iqbal</h1>
 			</a>
 		</div>
 	)
