f7c217f90e2702ef235de3c2b09826d285c92a39
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2023-12-06 18:02:41 +0700

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -20,13 +20,13 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">CREATED BY</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/ekizr_/?hl=id" class="hover:underline">
-										EKIZR
+									<a href="https://www.instagram.com/ibl_y/?hl=id" class="hover:underline">
+										IQBAL
 									</a>
 								</li>
 								<li>
-									<a href="https://www.instagram.com/dafyall/?hl=id" class="hover:underline">
-										DAFYALL
+									<a href="https://www.instagram.com/x_artm_/?hl=id" class="hover:underline">
+										X ARTM
 									</a>
 								</li>
 							</ul>
@@ -40,8 +40,8 @@ const Footer = () => {
 									</a>
 								</li>
 								<li>
-									<a href="https://www.youtube.com/@11TKJ3" class="hover:underline">
-										Youtube
+									<a href="w.me/6283188140830" class="hover:underline">
+										Whatsapp
 									</a>
 								</li>
 							</ul>
@@ -60,7 +60,7 @@ const Footer = () => {
 				<div class="sm:flex sm:items-center sm:justify-between">
 					<div class="flex mt-4 justify-center sm:mt-0">
 						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas XI TKJ 3 | Di Kelola Oleh Siswa TKJ 3
+							© {new Date().getFullYear()} Kelas X SBKN | Di Kelola Oleh Iqbal
 						</p>
 					</div>
 				</div>
