25fa48ce185425b7e0a470fe45714de9eabbfd28
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2023-12-06 22:53:37 +0700

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -20,13 +20,13 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">CREATED BY</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/ekizr_/?hl=id" class="hover:underline">
-										EKIZR
+									<a href="https://www.instagram.com/ibl_y/?hl=id" class="hover:underline">
+										iqbal
 									</a>
 								</li>
 								<li>
-									<a href="https://www.instagram.com/dafyall/?hl=id" class="hover:underline">
-										DAFYALL
+									<a href="https://www.instagram.com/x_artm_/?hl=id" class="hover:underline">
+										X ARTM
 									</a>
 								</li>
 							</ul>
@@ -35,13 +35,13 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">Follow us</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/acetkjthree/?hl=id" class="hover:underline ">
+									<a href="https://www.instagram.com/ibl_y/?hl=id" class="hover:underline ">
 										Instagram
 									</a>
 								</li>
 								<li>
-									<a href="https://www.youtube.com/@11TKJ3" class="hover:underline">
-										Youtube
+									<a href="wa.me/6283188140830" class="hover:underline">
+										Whatsapp
 									</a>
 								</li>
 							</ul>
